0acd827bd55da658f325560d4fd582273e833278
phillip-tech <pm.bogopane@gmail.com>
2024-10-03 09:23:45 +0200

    updated next.config.mjs with outputDir and reformatted code

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['next-ecommerce-api.vercel.app', 'cdn.dummyjson.com'],
-    },
-  };
-  
-  export default nextConfig;
+  outputDir: 'dist',
+  images: {
+    domains: ['next-ecommerce-api.vercel.app', 'cdn.dummyjson.com'],
+  },
+};
+
+export default nextConfig;
