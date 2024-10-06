ab334a2bba0a86eeb1c7067bc1bf4f5f5ae0f432
phillip-tech <pm.bogopane@gmail.com>
2024-10-06 13:39:26 +0200

    updated next.config.mjs by removing outputDir and reformatting images domains configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputDir: 'dist',
-  images: {
-    domains: ['next-ecommerce-api.vercel.app', 'cdn.dummyjson.com'],
-  },
-};
-
-export default nextConfig;
+    images: {
+      domains: ['next-ecommerce-api.vercel.app', 'cdn.dummyjson.com'],
+    },
+  };
+  
+  export default nextConfig;
