3da6998f0c681b15766e4b6358bd2bf1d86b99d9
phillip-tech <pm.bogopane@gmail.com>
2024-10-15 23:48:47 +0200

    Updated comments and added type annotations in products/[id]/route.js

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -2,13 +2,17 @@ import { db } from '../../../lib/firebase'; // Import Firestore instance
 import { doc, getDoc } from 'firebase/firestore'; // Firestore functions for client SDK
 import { NextResponse } from 'next/server';
 
+
 /**
  * GET /api/products/:id
  * 
- * Fetches a product from Firestore using the provided ID in the URL params.
- * Returns the product data along with its document ID as a JSON response.
- * If the product does not exist, returns a 404 error with an error message.
- * If there is an error in fetching the product, returns a 500 error with an error message.
+ * Fetches a product by ID from Firestore and returns the product data with the document ID.
+ * 
+ * @param {import('next/server').NextRequest} request - The request object from Next.js.
+ * @param {import('next/server').NextPageContext} context - The page context object from Next.js.
+ * @param {{ id: string }} params - The URL parameters object containing the product ID.
+ * 
+ * @returns {import('next/server').NextResponse} - The response object from Next.js containing the product data or an error message.
  */
 export async function GET(request, { params }) {
   let { id } = params; // Extract product ID from URL params
